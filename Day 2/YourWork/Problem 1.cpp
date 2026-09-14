0fc3c57df3d7ec8e9e474478d6518e647c15b69a
Raisa-251-115-088 <tabassumr861@gmail.com>
2026-09-14 14:21:18 +0600

    Add files via upload

---
+++ Day 2/YourWork/Problem 1.cpp
@@ -0,0 +1,13 @@
+#include<stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int, int> P1;
+    p1.first=10;
+    p2.second=20;
+    
+    cout<< "first value:" << p1.first <<"\n";
+    cout<< "second value" << p2.second <<"\n";
+
+    return 0;
+}
