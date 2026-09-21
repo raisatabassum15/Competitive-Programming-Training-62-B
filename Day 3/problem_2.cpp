cd3a6d600b114618e9a40f85092d2e9aa21c424a
Raisa-251-115-088 <tabassumr861@gmail.com>
2026-09-21 13:35:07 +0600

    Add vector reversal and output in problem_2.cpp
    Implemented a program to reverse a vector of integers and print the result.

---
+++ Day 3/problem_2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30, 40, 50};
+    int n = v.size();
+    
+    
+    for(int i = 0; i < n / 2; i++) {
+        int temp = v[i];
+        v[i] = v[n - 1 - i];
+        v[n - 1 - i] = temp;
+        
+    }
+		 for (int i=0;i < n;i++)
+    {
+			    cout << v[i] << endl;
+    }
+    
+    // It is now reversed!
+    return 0;
+}
