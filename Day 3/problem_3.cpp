98b4ced82f9d84f74aee98d1eb791ff89dccb41f
Raisa-251-115-088 <tabassumr861@gmail.com>
2026-09-21 13:35:40 +0600

    Add main function to print vector elements

---
+++ Day 3/problem_3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5};
+    
+   
+    for(int x : v) {
+        cout << x << " ";
+    }
+    cout << "\n";
+    
+    return 0;
+}
